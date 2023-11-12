191f4543cf77f8ef806bb7a405a413649449ffa1
charlieop <spchanae@connect.ust.hk>
2023-11-12 22:09:55 +0800

    refactored gamemanager code, fixed the bug in game scene, and refactored sound

--- src/main/java/com/example/comp3111f23g05/manager/AudioManager.java
+++ src/main/java/com/example/comp3111f23g05/manager/AudioManager.java
@@ -8,17 +8,6 @@ public class AudioManager {
     private static final int MAX_LOOP_TIME = 20;
     private Clip[] clips = new Clip[Sound.values().length]; //store the looping clip
     private static final AudioManager instance = new AudioManager();
-    private final String[] soundFiles = {
-            "/sounds/alert.wav",
-            "/sounds/block flick.wav",
-            "/sounds/button.wav",
-            "/sounds/done.wav",
-            "/sounds/themeSong.wav",
-            "/sounds/gameSong.wav",
-            "/sounds/Jerry.wav",
-            "/sounds/victory.wav",
-            "/sounds/failure.wav"
-    };
 
     private AudioManager() {}
     public static AudioManager getInstance() {
@@ -32,11 +21,7 @@ public class AudioManager {
             return;
         }
 
-        String fileName = soundFiles[sound.ordinal()];
-        URL url = AudioManager.class.getResource(fileName);
-        if(url == null){
-            System.out.println("can not find file named: " + fileName);
-        }
+        URL url = AudioManager.class.getResource(sound.url);
         try {
             AudioInputStream ais = AudioSystem.getAudioInputStream(url);
             DataLine.Info info = new DataLine.Info(Clip.class, ais.getFormat());
@@ -52,8 +37,7 @@ public class AudioManager {
             clip.loop(MAX_LOOP_TIME);
             this.clips[sound.ordinal()] = clip;
 
-        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
-            throw new RuntimeException(e);
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ignored) {
         }
     }
 
