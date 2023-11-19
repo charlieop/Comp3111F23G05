9f67845172e594dc5505479b63c32a15e7b1915f
slyipae1 <slyipae@ust.hk>
2023-11-19 09:48:36 +0800

    modify AudioManager Test case

--- src/test/java/com/example/comp3111f23g05/manager/AudioManagerTest.java
+++ src/test/java/com/example/comp3111f23g05/manager/AudioManagerTest.java
@@ -7,30 +7,31 @@ class AudioManagerTest {
     AudioManager audioManager = AudioManager.getInstance();
     @Test
     public void BGMTest() throws InterruptedException {
-        // all are target functions
         // bgm sound, with looping and stop
 
-        audioManager.play(Sound.THEME, true);
+        audioManager.play(Sound.THEME, true); //target function
         // multiple calls of play() on the same sound
-        audioManager.play(Sound.THEME, true);
+        audioManager.play(Sound.THEME, true); //target function
         Thread.sleep(1000);
-        audioManager.stop(Sound.THEME);
+        audioManager.stop(Sound.THEME); //target function
         Thread.sleep(1000);
         // call stop() when the sound is not playing
-        audioManager.stop(Sound.THEME);
+        audioManager.stop(Sound.THEME); //target function
     }
 
     @Test
     public  void SpecialTestCase(){
         // call stop() when haven't play a sound
-        audioManager.stop(Sound.GAME);
+        audioManager.stop(Sound.GAME); //target function
+        // invalid url
+        audioManager.play(Sound.INVALID_TEST, false); //target function
     }
 
     @Test
     public  void instantTest(){
         // instant sound effect
-        audioManager.play(Sound.BUTTON, false);
-        audioManager.play(Sound.JERRY, false);
+        audioManager.play(Sound.BUTTON, false); //target function
+        audioManager.play(Sound.JERRY, false); //target function
 
     }
 
