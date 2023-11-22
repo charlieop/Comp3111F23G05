ed5e68370f2af52dac919f0ba60196b81b358779
summyyyy <sylamau@connect.ust.hk>
2023-11-22 16:34:40 +0800

    add the specification in the GameManagerRefreshTest

--- src/test/java/com/example/comp3111f23g05/manager/GameManagerRefreshTest.java
+++ src/test/java/com/example/comp3111f23g05/manager/GameManagerRefreshTest.java
@@ -5,6 +5,17 @@ import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
 public class GameManagerRefreshTest extends ApplicationTest {
+
+    //Specification
+    // There are two methods that are responsible to handle the users' keycode input
+    // The first one is a private function in gameManager class which is named "keycodeProcess()".
+    // And it will only be called by the following handle() function.
+    // The second one is a public function in Refresh class, which is named handle() function.
+    // This design is to encode the users' input keycode more convenient
+    // And the keycodeProcess() function is only called by this handle() function.
+    // The handle() function does not call other function except this keycodeProcess() function
+    // Since they are closely related and responsible for the same functionality, we test they as a whole
+
     @Override
     public void start(javafx.stage.Stage stage) {
         SceneManager.getInstance().init(stage);
