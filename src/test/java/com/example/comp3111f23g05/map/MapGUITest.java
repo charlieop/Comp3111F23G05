1d3fecbb84660a58b3b5d5d6b8106275193b6671
slyipae1 <slyipae@ust.hk>
2023-11-22 18:09:13 +0800

    map error catch test case

--- src/test/java/com/example/comp3111f23g05/map/MapGUITest.java
+++ src/test/java/com/example/comp3111f23g05/map/MapGUITest.java
@@ -8,7 +8,7 @@ import org.testfx.framework.junit.ApplicationTest;
 public class MapGUITest extends ApplicationTest {
     @Test
     public void constructorTest() {
-        Map map = new Map();
+        Map map = new Map("MazeMap.csv");
         Coordinate[] path = GameManager.getInstance().CalculateShortestPath(map, map.entryPos, map.exitPos);
         MapGUI gui = new MapGUI(map, new Coordinate[0]); // target function
         MapGUI guiWithPath = new MapGUI(map, path); // target function
